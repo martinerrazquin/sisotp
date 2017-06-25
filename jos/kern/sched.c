bbac7e787e371aeb4af3aa2dc337c51b90b89622
Martin Errazquin <martin.errazquin@hotmail.com>
2017-06-25 05:29:58 -0300

    mejor

--- jos/kern/sched.c
+++ jos/kern/sched.c
@@ -37,7 +37,14 @@ sched_yield(void)
 			break;
 		}
 	}
-	//MARTIN SCHED_YIELD: esto es asi para que nunca vuelve, pero solo por el noreturn. En task.c no era asi! Llevara a bugs???
+	//MARTIN SCHED_YIELD: una vez que ande lo de arriba se puede cambiar todo por solo lo de abajo
+/*
+	for (idle = curenv+1 ; idle != curenv; idle++){
+		if(idle >= envs + NENV) idle = envs;
+		if(idle->env_status == ENV_RUNNABLE) break;
+	}
+	
+*/
 	if(idle != curenv || curenv->env_status == ENV_RUNNING){
 		env_run(idle);
 	}
